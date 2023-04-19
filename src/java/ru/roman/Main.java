e9640ed8040878548bbdfbd44e2cf6a1f36215b1
roma <rommur5@mail.ru>
2023-04-19 21:49:20 +0300

    5 задание

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -6,10 +6,13 @@ public class Main {
         int total=salary;
         int month=1;
         do {
-            System.out.println("Месяц "+month+" сумма накоплений "+total);
+            if(month%6==0) {
+                System.out.println("Месяц " + month + " сумма накоплений " + total);
+            }
             month=month+1;
             total=total+salary;
             total=total+(total/100)*7;
+
         }while (total<=12_000_000);
     }
 }
