795461196a6276b4b6171a295255d5a1c2fbeffd
roma <rommur5@mail.ru>
2023-04-19 20:08:41 +0300

    1 задание

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -2,5 +2,13 @@ package ru.roman;
 
 public class Main {
     public static void main(String[] args) {
+        int salary = 15_000;
+        int total = 0;
+        int i = 0;
+        while (total < 2_459_000) {
+            i = i + 1;
+            total = total + salary;
+            System.out.println("Месяц " + i + ", Сумма накоплений равна " + total);
+        }
     }
 }
