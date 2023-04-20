c87466c0392b40d5c17ce867f531d71e62c9d059
roma <rommur5@mail.ru>
2023-04-20 16:33:15 +0300

    7 задание убрал лишнее

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -2,8 +2,6 @@ package ru.roman;
 
 public class Main {
     public static void main(String[] args) {
-        int saterday;
-        int saterdayMonth=7;
         int i=2;
         while (i<=31){
                 System.out.println("Сегодня пятница " + i + "-е число. Необходимо подготовить отчет.");
