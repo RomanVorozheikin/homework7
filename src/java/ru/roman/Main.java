5d4739351c18e5099f634066f095388725b28396
roma <rommur5@mail.ru>
2023-04-20 16:31:55 +0300

    7 задание

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -2,18 +2,12 @@ package ru.roman;
 
 public class Main {
     public static void main(String[] args) {
-        int salary = 15_000;
-        int total = 0;
-        int year = 9;
-        int month = year * 12;
-        int i=1;
-        for (; i <= month; i++) {
-
-            total = total + salary;
-            total = total + (total / 100) * 7;
-            if (i % 6 == 0) {
-                System.out.println("Месяц " + i + " накопленная сумма " + total);
-            }
+        int saterday;
+        int saterdayMonth=7;
+        int i=2;
+        while (i<=31){
+                System.out.println("Сегодня пятница " + i + "-е число. Необходимо подготовить отчет.");
+            i=i+7;
         }
     }
 }
