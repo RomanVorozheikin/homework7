f3d6fb269267a765a07458cf7860fbfa8330ea4d
roma <rommur5@mail.ru>
2023-04-20 16:57:30 +0300

    8 задание

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -2,10 +2,16 @@ package ru.roman;
 
 public class Main {
     public static void main(String[] args) {
-        int i=2;
-        while (i<=31){
-                System.out.println("Сегодня пятница " + i + "-е число. Необходимо подготовить отчет.");
-            i=i+7;
+        int year = 2023;
+        int past = year - 200;
+        int future = year + 100;
+        int i = 0;
+        while (i < future) {
+            i = i + 79;
+
+            if (i > past&&i<future) {
+                System.out.println(i);
+            }
         }
     }
 }
